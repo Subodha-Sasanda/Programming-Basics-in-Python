f697c268aec00bfe9a27c336562cc3fa2327d07d
Subodha Sasanda Withana Arachchi <subodha.sasandha@gmail.com>
2022-10-23 22:26:10 +0530

    Create Tutorial 01.py

---
+++ Week 01/Tutorial 01.py
@@ -0,0 +1,78 @@
+pounds = 25           
+pence = pounds*100                          #answer goes to the left
+print("25 pounds in pence is",pence)         #print answer
+
+kilometers = 5
+meteres = kilometers*1000
+print("5 kilometers in meters is", meteres)
+
+cost = 10
+postage = 2
+total = cost+ postage
+print("total is", total)
+
+count = 0
+count =count + 2 #add 2 on to count and store answer in count
+count =count + 2
+count =count + 2
+count =count + 2
+print("Total is",count)
+
+
+item1 = 4
+item2 = 6
+item3 = item1+item2
+print("total=",item3)
+
+meal = 56
+tip = meal/10
+print( "10% tip is=", tip)
+
+print(2+4)
+print(2.5+4.2)
+print(6-4)
+print(6.0 - 4.5)
+print(6 * 3)
+print(6 / 3)
+print(6 % 3)
+print(6 // 3) # floor division: always truncates fractional remainders
+print(-5)
+print(3**2)   # three to the power of 2
+print(6.0 - 4.5)
+print(6 * 3)
+print(6 / 3)
+print(6 % 3)
+print(6 // 3) # floor division: always truncates fractional remainders
+print(-5)
+print(3**2)   # three to the power of 2
+
+print(2 + 4.2)
+print(6 - 4.0)
+print(6 * 3.0)
+print(6 / 3.0)
+print(6 % 3.0)
+print(6 // 3.0) # floor division: always truncates fractional remainders
+print(-5.0)
+print(3**2.0)   # three to the power of 2
+
+print("10(a)")
+num = 5
+print(num)		
+num = num + 2 
+print(num) 		
+num = num // 3 * 6
+print(num)		
+print(7 + 15 % 4)		
+num = 24 // 3 // 4
+print(num)		
+
+print("String assignment")
+a = "Hello out there."
+print(a)
+b = "hello again"
+print(b)
+print(a+b)
+
+
+
+
